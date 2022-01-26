4a02e7a468f9acf627c878bf9c1a066b1bc055bc
kien.vuong <kien.vuong@ant-tech.asia>
2022-01-26 20:41:30 +0700

    add update rank

--- app/models/index.js
+++ app/models/index.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.json')[env];
 const db = {};
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(config.database, config.username, config.password, {...config, logging: console.log})
+const sequelize = new Sequelize(config.database, config.username, config.password, {...config, logging: false})
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
